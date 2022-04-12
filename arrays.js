a88c90604561750301b6c27171cf9d449df9dae8
Hunter Saunders <hjvs98@gmail.com>
2022-04-12 16:30:09 -0600

    Initial Commit

--- arrays.js
+++ arrays.js
@@ -2,3 +2,13 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+let array = [7, 12, -4, "hello"];
+
+// for (let i = 0; i < array.length; i++) {
+//   array[i] += 10;
+//   console.log(array[i]);
+// }
+
+for (let i = array.length - 1; i >= 0; i--) {
+  console.log(array[i]);
+}
